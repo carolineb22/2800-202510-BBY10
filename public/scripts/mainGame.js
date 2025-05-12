4f1eccee9ccaf1facb5aac86fdcf50624ef43503
Glenn Dossot <glenn@dossot.net>
2025-05-12 09:41:31 -0700

    Created Building object.

--- public/scripts/mainGame.js
+++ public/scripts/mainGame.js
@@ -50,6 +50,46 @@ function GeographicalElement(id, name, passiveProduction, situationalBuildings, 
     }
 }
 
+function Building(id, type, name, consumptionArray, productionArray, costArray, depletion, needsType, builtOnElement) {
+    this.uuid = crypto.randomUUID(); //uuid of this element
+    this.id = id; //id of this element, is seperate from uuid as multiple of the same building can inhabit an element
+    this.type = type; //type of building, used for seperation into categories for build meny
+    this.name = name; //display name of this element
+    this.consumptionArray = consumptionArray; //array of arrays that contain a resource and amount to be used per tick
+    this.productionArray = productionArray; //array of arrays that contain a resource and amount to produce per tick
+    this.costArray = costArray; //array of arrays that contain what ResourceTypes to use and their amount
+    this.builtOnElement = builtOnElement; //which geographical element is this building built on?
+
+    this.doesDeplete = false; //this building does not deplete the resource of what its built on
+    if (depletion) {
+        this.doesDeplete = true; //nvm it does deplete 
+        this.depletion = depletion; //amount to deplete by
+    }
+
+    if (needsType) {
+        this.needsType = needsType;
+    } 
+    this.doTick = function() {
+        this.productionArray.forEach(val => {
+            Resources[val[0]] += val[1]
+        })
+        this.consumptionArray.forEach(val => {
+            Resources[val[0]] -= val[1]
+        })
+        if (this.doesDeplete) {
+            getGeographicalElementById(this.builtOnElement).depleteBy(this.depletion);
+        }
+    }
+
+    this.getFormattedString = function() {
+        return `${this.name}, ${this.type}
+        ${Array.isArray(this.consumptionArray) && this.consumptionArray.length ? `<br>  Consumes: ${this.consumptionArray.map(e => `<br>${ResourceTypes[e[0]]}, ${e[1]}/tick`).join()}` : ""}
+        ${Array.isArray(this.productionArray) && this.productionArray.length ? `<br>  Produces: ${this.productionArray.map(e => `<br>${ResourceTypes[e[0]]}, ${e[1]}/tick`).join()}` : ""}
+        `
+    }
+    
+}
+
 // GAME LOOP -----------------------------------------------------------------
 function gameLoop() {
     updateResources();
